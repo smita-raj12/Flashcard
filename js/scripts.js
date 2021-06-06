df423946a86dcbda50c2c9e5322f2c175f16f938
smita Raj <smita.raj12@gmail.com>
2021-06-06 12:33:07 -0700

    added more content on flashcard website

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,53 @@
 $(document).ready(function() {
   $(".click").click(function() {
     $(".text-showing").toggle();
-    $(".text-hidden").fadeIn();
-    
   });
+  $(".oclick").click(function() {
+    $(".otext-showing").toggle();
+  });
+  $(".vclick").click(function() {
+    $(".vtext-showing").toggle();
+  });  
+  $(".fclick").click(function() {
+    $(".ftext-showing").toggle();
+  });  
+  $(".mclick").click(function() {
+    $(".mtext-showing").toggle();
+  });  
+  $(".aclick").click(function() {
+    $(".atext-showing").toggle();
+  });  
+  $(".pclick").click(function() {
+    $(".ptext-showing").toggle();
+  });  
+  $(".rclick").click(function() {
+    $(".rtext-showing").toggle();
+  });  
+  $(".cclick").click(function() {
+    $(".ctext-showing").toggle();
+  });  
+  $(".sclick").click(function() {
+    $(".stext-showing").toggle();
+  });  
+  $(".bclick").click(function() {
+    $(".btext-showing").toggle();
+  });  
+  $(".uclick").click(function() {
+    $(".utext-showing").toggle();
+  });  
+  $(".nclick").click(function() {
+    $(".ntext-showing").toggle();
+  });  
+  $(".nuclick").click(function() {
+    $(".nutext-showing").toggle();
+  });  
+  $(".alclick").click(function() {
+    $(".altext-showing").toggle();
+  });  
+  $(".coclick").click(function() {
+    $(".cotext-showing").toggle();
+  });  
+  $(".jqclick").click(function() {
+    $(".jqtext-showing").toggle();
+  });  
 });
-
